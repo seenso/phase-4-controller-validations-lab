fec1fe2f6e6fcd006a9a3e2e96effa8f8165883d
seenso <>
2022-01-12 13:31:30 -0900

    Completed

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -2,14 +2,15 @@ class AuthorsController < ApplicationController
   
   def show
     author = Author.find(params[:id])
-
     render json: author
   end
 
   def create
-    author = Author.create(author_params)
-
+    author = Author.create!(author_params)
     render json: author, status: :created
+    # send back err messages if the new author is invalid
+  rescue ActiveRecord::RecordInvalid => invalid
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
   end
 
   private
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,16 +2,16 @@ class PostsController < ApplicationController
 
   def show
     post = Post.find(params[:id])
-    
     render json: post
   end
 
   def update
     post = Post.find(params[:id])
-
-    post.update(post_params)
-
+    post.update!(post_params)
     render json: post
+    # send back err messages if the updated post is invalid
+  rescue ActiveRecord::RecordInvalid =>  invalid 
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
   end
 
   private
